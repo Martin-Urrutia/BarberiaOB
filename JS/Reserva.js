c8f08fe2d9d3d64d85f3a197520080c7295e14f6
Martin-Urrutia <martinurrucsa@gmail.com>
2025-07-24 08:32:54 -0300

    initial commit

---
+++ JS/Reserva.js
@@ -0,0 +1,11 @@
+class Reserva {
+    constructor(_nombre,_apellido,_celular,_correo,_barberoId,_servicioId,_fecha){
+        this.nombre=_nombre;
+        this.apellido=_apellido;
+        this.celular=_celular;
+        this.correo=_correo;
+        this.barberoId=_barberoId;
+        this.servicioId=_servicioId;
+        this.fecha=_fecha;
+    }
+}
